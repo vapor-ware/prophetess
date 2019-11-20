2150d1845c520f4d6700cc3f810d8ffb2a9c3a30
Marco Ceppi <marco@ceppi.net>
2019-11-19 21:56:14 -0500

    :building_construction: Allow config to specify class

--- prophetess/utils.py
+++ prophetess/utils.py
@@ -14,7 +14,7 @@ def build_plugin(plugin_type, plugin_id, plugin_config):
     if module_name not in config.PLUGINS:
         raise InvalidPlugin(f'{plugin_name} not found, try `pip install prophetess-{module_name}`?')
 
-    name = '{}{}'.format(plugin_name, plugin_type)
+    name = plugin_config.get('class', '{}{}'.format(plugin_name, plugin_type))
     plugin = getattr(config.PLUGINS.get(module_name), name)
 
     return plugin(id=plugin_id, config=plugin_config.get('config'), labels=(plugin_name, plugin_type, name))
